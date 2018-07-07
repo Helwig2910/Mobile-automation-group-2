3c331e6dddda6cc98d59b71e4d0c70644968bd55
Konstantinov Oleg <padrehelwig@gmail.com>
2018-07-07 18:54:11 +0300

    Little change Ex7

--- JavaAppiumAutomation/src/Ex7.java
+++ JavaAppiumAutomation/src/Ex7.java
@@ -1,20 +1,9 @@
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
-import lib.ui.MainPageObject;
 import lib.ui.SearchPageObject;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class Ex7 extends CoreTestCase {
-
-    private MainPageObject MainPageObject;
-
-    protected void setUp() throws Exception
-    {
-        super.setUp();
-
-        MainPageObject = new MainPageObject(driver);
-    }
 
     @Test
     public void testChangeScreenOrientation()
@@ -31,7 +20,7 @@ public class Ex7 extends CoreTestCase {
 
         String tittle_after_rotation = ArticlePageObject.getArticleTittle();
 
-        Assert.assertEquals(
+        assertEquals(
                 "Article tittle have been changed after screen rotation",
                 tittle_before_rotation,
                 tittle_after_rotation
